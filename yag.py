ff54d626384b31a4b41f71443c08478044565d4a
ezxpro <ezequiel.ezb@gmail.com>
2022-09-26 12:20:45 -0300

    renamed to yag and added license

--- yag.py
+++ yag.py
@@ -1,1 +1,8 @@
-# empty pygit file
+import os
+
+# empty yag file
+def init(repo):
+    """Criar diretório para o repositório e inicializa
+    o diretório .git"""
+    os.mkdir(repo)
+    os.mkdir(os.path.join(repo, '.git'))
