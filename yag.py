2900d736e40f7d3293d6f1e867b755c12a8bf74c
ezxpro <ezequiel.ezb@gmail.com>
2022-09-28 22:07:03 -0300

    comentários e variáveis em português

--- yag.py
+++ yag.py
@@ -1,9 +1,16 @@
 import os
 
-# empty yag file
+def read_file(path):
+    """Lê os conteúdos dos arquivos, dado um caminho em bytes"""
+
+def write_file(path, data):
+    """"""
+
 def init(repo):
     """Criar diretório para o repositório e inicializa
     o diretório .git"""
-    
     os.mkdir(repo)
     os.mkdir(os.path.join(repo, '.git'))
+    for name in ['objects', 'refs', 'refs/heads']:
+        os.mkdir(os.path.join(repo, '.git', name))
+    write_file(os.path.join(repo, '.git', 'HEAD'))
