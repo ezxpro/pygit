f8b6d35b7cbbb9a1af36e9f5c7953880052b2d85
ezxpro <ezequiel.ezb@gmail.com>
2022-09-30 15:37:38 -0300

    adicionado função hash_object()

--- yag.py
+++ yag.py
@@ -12,7 +12,6 @@ def escrever_arquivo(caminho, dados):
         return f.write(dados)
 
 
-
 def init(repo):
     """Criar diretório para o repositório e inicializa
     o diretório .git"""
@@ -21,3 +20,18 @@ def init(repo):
     for nome in ['objects', 'refs', 'refs/heads']:
         os.mkdir(os.path.join(repo, '.git', nome))
     escrever_arquivo(os.path.join(repo, '.git', 'HEAD'))
+
+
+def hash_object(data, obj_type, write=True):
+    """Compute hash of object data of given type and write to object store
+    if "write" is True. Return SHA-1 object hash as hex string.
+    """
+    header = '{} {}'.format(obj_type, len(data)).encode()
+    full_data = header + b'\x00' + data
+    sha1 = hashlib.sha1(full_data).hexdigest()
+    if write:
+        path = os.path.join('.git', 'objects', sha1[:2], sha1[2:])
+        if not os.path.exists(path):
+            os.makedirs(os.path.dirname(path), exist_ok=True)
+            write_file(path, zlib.compress(full_data))
+    return sha1
